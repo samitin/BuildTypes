29847baddaf6534b4a225dccc5904e3e43228633
evgenij <samitin.e@gmail.com>
2022-05-17 15:40:59 +1100

    варианты построений приложения

--- app/src/main/java/ru/samitin/buildtypes/MainActivity.kt
+++ app/src/main/java/ru/samitin/buildtypes/MainActivity.kt
@@ -2,10 +2,15 @@ package ru.samitin.buildtypes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.samitin.buildtypes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.textView.text = BuildConfig.MY_BUILD_TYPE
     }
 }
